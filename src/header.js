16853b5d2c2fa3462276f72144eeba5876aad691
EthanCopeland <ethancopeland760@gmail.com>
2021-02-22 17:41:07 +0000

    added nav bar, homepage, logo

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.png';
+
+function Navbar() {
+	return (
+		<header>
+            <img src={logo} className="logo" alt="logo" />
+		</header>
+	)
+}
+
+export default Navbar;
