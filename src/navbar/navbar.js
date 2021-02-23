d71e3021542b6fb93efc51efb3545c57955a6bdc
TheMGRF <thomas.grf@googlemail.com>
2021-02-23 15:53:33 +0000

    Add chat and move support to rules

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
 		<div id="nav-box">
 			<ul id="navbar" className="pure-u-1-3">
 				<li><a className="active" href="/"><i className="fas fa-home"></i> Home</a></li>
-				<li><a href="/blog"><i className="fas fa-comment-dots"></i> Blog</a></li>
+				<li><a href="/blog"><i className="fas fa-book"></i> Blog</a></li>
 				<li><a href="/leaderboards"><i className="fas fa-chart-bar"></i> Leaderboards</a></li>
 				<li><a href="/rules"><i className="fas fa-scroll"></i> Rules</a></li>
-				<li><a href="/support"><i className="fas fa-question-circle"></i> Support</a></li>
+				<li><a href="/chat"><i className="fas fa-comments"></i> Chat</a></li>
 				<li><a href="/map"><i className="fas fa-map-marked-alt"></i> World Map</a></li>
 				<li><a id="store" href="https://store.minecrossing.xyz"><i className="fas fa-shopping-cart"></i> Store</a></li>
 				<li><a href="/login"><i className="fas fa-sign-in-alt"></i> Login</a></li>
