0ab77e257d1213e918b76640c6c0fc19e2e17333
TheMGRF <thomas.grf@googlemail.com>
2021-02-26 15:40:47 +0000

    Send web messages, reset chat box and scroll down constantly

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -7,12 +7,57 @@ export default class Chat extends React.Component {
         super(props);
 
         this.state = {
+            typed: "",
             data: []
         }
+
+        this.updateText = this.updateText.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.sendMessageEnter = this.sendMessageEnter.bind(this);
+    }
+
+    updateText(msg) {
+        this.setState({
+            typed: msg
+        });
+    }
+
+    sendMessageEnter(e) {
+        if (e.key === "Enter") {
+            this.sendMessage();
+        }
+    }
+
+    sendMessage() {
+        let inputBox = document.getElementById("chat-entry");
+        let msg = inputBox.value;
+
+        // Reset
+        inputBox.value = "";
+
+        console.log("Sending chat message: "+ msg);
+
+        // Send message to API
+        let url = "https://api.minecrossing.xyz/chat/send";
+
+        let cache = [];
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ message : msg })
+        })
     }
 
     render() {
         let chat = this.state.data;
+
+        // always scroll to bottom of chat box
+        let scrollBox = document.getElementById("chat-box");
+        scrollBox.scrollTop = scrollBox.scrollHeight - scrollBox.clientHeight;
 
         return (
             <div id="chat-container">
@@ -24,8 +69,8 @@ export default class Chat extends React.Component {
                     }
                 </div>
                 
-                <input id="chat-entry" placeholder="> message"></input>
-                <button id="send-chat">Send</button>
+                <input id="chat-entry" placeholder="> message" onChange={this.updateText} onKeyDown={this.sendMessageEnter}></input>
+                <button id="send-chat" onClick={this.sendMessage}>Send</button>
             </div>
         )
     }
