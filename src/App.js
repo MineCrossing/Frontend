68c556f34ea0266e914640f819835910ed8a6ac8
EthanCopeland <ethancopeland760@gmail.com>
2021-03-03 21:23:30 +0000

    added rules

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Homepage from './homepage/homepage.js';
 import Header from './header/header.js';
 import Footer from './footer/footer.js';
 import Map from './map/map.js';
+import Leaderboard from './leaderboard/leaderboard.js';
 import Rules from './rules/rules.js';
 import Chat from './chat/chat.js';
 import PageNotFound from './PageNotFound.js';
@@ -26,7 +27,7 @@ function App() {
                     </Route>
                     <Route exact path="/leaderboards">
                         <Nav page="leaderboards" />
-                        {/* <Leaderboards/> */}
+                        <Leaderboard />
                     </Route>
                     <Route exact path="/rules">
                         <Nav page="rules" />
--- src/rules/rules.js
+++ src/rules/rules.js
@@ -2,20 +2,21 @@ import React from 'react';
 import "../homepage/homepage.css";
 import "./rules.css";
 
-function Rules() {
+
+		function Rules(){
 	return (
-		<div id="homepage">
+		<div id="rules">
 			<div id="content" className="pure-u-3-5">
 
 				<h1 id="rules-h1">Rules and Policies!</h1>
 
 				<p id="welcome-paragraph">
 					Welcome to the rules and policies page, this page has been made to make sure that all of our users can enjoy their time in MineCrossing and that it is fair for everyone. Therefore we have put the rules in place for each member of our growing community to follow, if you are to be caught breaking the rules this can lead to being banned.
-        </p>
+        		</p>
 
 				<p id="welcome-paragraph">
 					Please keep in mind that rules can be subjected to change for either adding new rules, removing old rules, or editing rules to be more appropriate for users. Within moderation, admins can enforce the rules if they believe users are not following regulations, and finally the rules may be interchangeable between the platforms of MineCrossing such as the chat rules and the game rules.<br /> <br />Please Use the links below to navigate thorughout the page to view the contents.
-		</p>
+				</p>
 
 				<hr />
 				<br />
@@ -24,32 +25,120 @@ function Rules() {
 					<li><a href="#mc-rules">1.1 MineCrossing Rules</ a></li><br />
 					<li><a href="#chat-rules">1.2 Chat Rules</ a></li><br />
 					<li><a href="#store-rules">1.3 Store Policies</ a></li><br />
-					<li><a href="#tos">1.4 Terms of Service</ a></li><br />
-					<li><a href="#pp">1.5 Privacy Policy</ a></li><br />
-					<li><a href="#help">1.6 Support</ a></li><br />
+					<li><a href="#support">1.6 Support</ a></li><br />
+					<li><a href="#service">1.4 Terms of Service</ a></li><br />
+					<li><a href="#privacy">1.5 Privacy Policy</ a></li><br />
 				</ul>
 
 				<br />
 				<hr />
 
+
 				<h2 id="mc-rules">MineCrossing Rules</h2>
-				<p id="content-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at bibendum nulla. Curabitur a dolor ac dui dictum iaculis. Nullam ut est vitae enim euismod condimentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean aliquet libero nibh, in laoreet ante mattis quis. Pellentesque condimentum sodales nisi, porttitor sodales libero. Sed vestibulum ante ipsum, id rutrum turpis aliquam eu. Integer vitae felis vel nibh eleifend posuere. Integer nec sollicitudin arcu. Duis id pharetra odio, ac egestas enim. Maecenas ultrices molestie mauris id porta. Sed quis metus ut dui vehicula fringilla. Nulla faucibus non arcu non tincidunt. Integer id nisl at enim commodo volutpat sed sed erat. Pellentesque posuere luctus pellentesque.</p>
+				<dl id="content-paragraph" className="pure-u-3-5">
+					<dt>1. Respect all players</dt>
+					<dd>- Respecting players means that all players should have a fair experience and not have negative intentions between each other.</dd>
+					<br />
+					<dt>2. No Griefing</dt>
+					<dd>- Griefing will not be tolerated between players to provide a positive in-game experience for players and an appropriate environment.</dd>
+					<br />
+					<dt>3. No Cheating</dt>
+					<dd>- Any use of macros, cheats, hacks and other variations of this is not allowed due to the unfair advantages given to players through this.</dd>
+					<br />
+					<dt>4. Player Safety</dt>
+					<dd>- Player safety is critical for our server, do NOT share any of your account information with anyone else, this is the player's responsibility.</dd>
+					<br />
+					<dt>5. Be PG friendly with creations</dt>
+					<dd>- Players have the control over in-game building, PG friendly creations are the only allowed concepts to prevent inappropriate themes for a wide range of ages.</dd>
+					<br />
+					<dt>6. No Scamming</dt>
+					<dd>- Any Players found attempting to scam other users will be banned, our goal is a positive environment where users can enjoy our game.</dd>
+					<br />
+					<dt>7. AFK limitations</dt>
+					<dd>- AFK farming is not allowed on the server due to the unfair advantage it provides, this includes any form of method used to kill mobs or bosses without input.</dd>
+					<br />
+					<dt>8. Do not abuse bugs or exploits</dt>
+					<dd>- Any bugs or exploits are features not mentioned within the specification, they will be limited but any bugs found should be reported and if abused by a player instead, will lead to a potential ban as it is an unfair advantage.</dd>
+					<br />
+					<dt>9. Do not impersonate staff</dt>
+					<dd>- Any impersonation of staff members in game for any reason will lead to a ban due to the risk of player safety, in the case of this contact support about any concern</dd>
+					<br />
+					<dt>10. No leaking of information</dt>
+					<dd>- Leaking information can be very dangerous depending on the sensititvity of it, therefore any leaks will not be tolerated due toi the user risk</dd>
+					<br />
+					<dt>11. Do not buy or sell in-game items for real money</dt>
+					<dd>- Buying or selling in-game earnable items for real money is not recommended due to the safety of the transaction and will be at the risk of the user </dd>
+				</dl>
+				
+				<hr />
 
 				<h2 id="chat-rules">Chat Rules</h2>
-				<p id="content-paragraph">Integer eget ipsum sed tortor faucibus pretium sit amet non nisl. Sed id quam eu felis ultricies hendrerit sed id elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras non orci interdum, iaculis sem sed, pellentesque ipsum. Morbi ornare laoreet neque vitae pulvinar. Fusce ullamcorper velit placerat turpis finibus, nec efficitur nulla consequat. Vivamus sit amet eleifend tortor. Proin facilisis, ipsum at pretium lobortis, metus tortor lacinia libero, id vestibulum turpis diam ac mi. Ut mi ante, dapibus a nisi eu, hendrerit posuere quam. Nulla sagittis dignissim erat, id fringilla ante bibendum a. In hac habitasse platea dictumst. In tempus sapien ex, tempus interdum erat fermentum at. Donec ut nibh condimentum, maximus lectus sed, pulvinar urna. Maecenas interdum, elit quis placerat tincidunt, nisi augue fermentum tellus, id sodales ipsum ex a tortor.</p>
+				<dl id="content-paragraph" className="pure-u-3-5">
+					<dt>1. Do not spam</dt>
+					<dd>- Don't spam messages in chat or messages with no purpose. </dd>
+					<br />
+					<dt>2. Limitations to advertising</dt>
+					<dd>- Streams and videos are fine in chat, but any links to other servers, merch stores, or referral links, etc. are not allowed.</dd>
+					<br />
+					<dt>3. No Public shaming</dt>
+					<dd>- Any public shaming is not tolerated in chat and will lead to bans if there is a continous thread of messages.</dd>
+					<br />
+					<dt>4. Username Guidelines</dt>
+					<dd>- Due to the age range of MineCrossing please keep usernames appropriate and PG friendly for everyone.</dd>
+					<br />
+					<dt>5. No inappropriate posts</dt>
+					<dd>- inappropriate posts are not allowed on the server as they create a negative impact and can lead to spam content.</dd>
+					<br />
+					<dt>6. No Political or Religious discussions</dt>
+					<dd>- In our server we would like to have fun and avoid political or Religious conversations due to the conflicts often arising from them, if found please contact support with the message snippets.</dd>
+					<br />
+					<dt>7. Do not flair arguments</dt>
+					<dd>- If any arguments occur in chat contact support to resolve the scenario, do not add flames to the argument or join in.</dd>
+					<br />
+					<dt>8. Do not harrass other users</dt>
+					<dd>- Harrassment of other players is not tolerated due to the negative impact, similar to public shaming this will leade to potential user bans.</dd>
+				</dl>
+
+				<hr />
 
 				<h2 id="store-rules">Store Policies</h2>
-				<p id="content-paragraph">Sed neque libero, eleifend sed blandit id, efficitur vel sapien. Etiam ut metus feugiat, consequat est sit amet, viverra velit. Praesent elit arcu, porttitor eget velit id, dapibus mollis ex. Aenean ipsum eros, molestie non nulla ac, mattis porta nunc. Pellentesque bibendum purus eu lacus tincidunt porta. Sed vitae suscipit elit. Ut vehicula at velit a congue. Pellentesque eu enim semper, sodales est a, tincidunt sapien. Donec vitae nisl auctor purus malesuada aliquet. Aliquam et enim nunc. Fusce auctor non diam non facilisis. Duis dapibus scelerisque lacinia. Mauris magna erat, auctor sed tellus non, laoreet laoreet turpis. Praesent vel neque sit amet dui iaculis efficitur non vitae diam. Cras eu orci ac arcu congue imperdiet ut a dolor.</p>
+				<dl id="content-paragraph" className="pure-u-3-5">
+					<dt>1. Refunds</dt>
+					<dd>- Similar to Minecraft we operate with a no refunds on our products and it is the customer responsibility when purchasing items. </dd>
+					<br />
+					<dt>2. Refunds on Banned users</dt>
+					<dd>- If a user has been banned they will not be refunded on any gems they possessed or items they had purchased in accordance with the no refund policy.</dd>
+					<br />
+					<dt>3. Customer responsibility</dt>
+					<dd>- The customer is responsible for any purchases made in store and MineCrossing cannot be held accountable for purchases, it is the responsibility of the customer to keep their information secure according to the security guidelines</dd>
+	
+				</dl>
 
-				<h2 id="help">Support</h2>
-				<p id="content-paragraph">For any support requirements please contact us at support@minecrossing.com explaining the issue. <br /> Responses may vary depending on the level of traffic within the support channels.</p>
+				<hr />
 
+				<h2 id="support" className="pure-u-3-5">Support</h2>
+				<p id="content-paragraph">For any in-game support or reports, please contact us at support@minecrossing.com <br />
+				Explain the issue with an image and description if possible. <br />Responses may vary depending on the level of traffic within the support channels.</p>
 
-				<h2 id="tos">Terms of Service</h2>
-				<p id="content-paragraph">Nullam vestibulum sagittis urna eu congue. Nam eleifend nisi sed sapien iaculis, ut pharetra sem pulvinar. Maecenas sed quam et odio mattis lacinia. Nullam lorem metus, convallis sit amet metus et, ultricies finibus mi. Quisque eget consectetur nisi, id tincidunt quam. Mauris eros diam, feugiat nec placerat at, ornare commodo quam. Quisque porta, erat quis fringilla interdum, lacus tellus semper odio, id venenatis mi lectus a leo. Nunc porta sollicitudin rutrum. Nam convallis sed urna accumsan dapibus. Proin tempus elementum lectus, vel semper nunc ornare et. Suspendisse varius rhoncus odio, vel dictum mi fringilla in. Proin vitae ullamcorper massa. Aliquam id ipsum pretium, varius lectus nec, venenatis arcu.</p>
+				<hr />
 
-				<h2 id="pp">Privacy Policy</h2>
-				<p id="content-paragraph">Maecenas varius ornare ipsum et maximus. Mauris sed tortor in diam euismod feugiat in in nunc. Donec id est semper, gravida nunc nec, consectetur nisi. Morbi turpis est, eleifend et justo at, auctor sodales est. Suspendisse quis leo egestas, tincidunt elit at, consectetur orci. Sed faucibus eget justo non tristique. Donec elementum arcu a neque faucibus, in tincidunt metus efficitur. Mauris quis mauris dignissim velit rutrum imperdiet id at quam. Donec iaculis elit quis dolor imperdiet porta. Nam hendrerit metus vitae gravida consequat. Suspendisse porta finibus feugiat. Aliquam id diam finibus, sollicitudin nisi sed, pellentesque mauris. Nulla facilisi. Etiam faucibus, ex in congue pellentesque, est arcu posuere sapien, in commodo magna leo quis leo. Nullam justo lacus, hendrerit a lectus sit amet, pharetra vestibulum risus. Morbi rutrum in augue vitae consectetur.</p>
+				<h2 id="service">Terms of Service</h2>
+				< a href="https://docs.google.com/document/d/e/2PACX-1vT-R4zo-9gcR_WBf4RZD887ox4-yqcGmq6E4aMh9dVi3kxeXYMjFEeP_hEOqNl0FfvEPzSRQ6EtXDxz/pub" target="_blank"> -- Click here for our Terms of Service --</a>
+				<br /><br />
+				<hr />
+
+				<h2 id="privacy">Privacy Policy</h2>
+				<a href="https://docs.google.com/document/d/e/2PACX-1vQyWI-cNev18GUO8yf7sIhAl6pe8w829u7QenpE8vF_0-omEek1KSXXnQavBCnbmjrqrzOVCTGy5cCK/pub" target="_blank">-- Click here to view our Privacy Policy --</a>
+				<br /><br />
+
+				<hr />
+				<br />
+
+				<ul id="rules-nav">
+					<li><a href="#">Back to the top</ a></li><br />
+				</ul>
+
+				<br />
 				<br />
 				
 			</div>
@@ -58,3 +147,4 @@ function Rules() {
 }
 
 export default Rules;
+
