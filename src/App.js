0dc94ced8e871c31327abf6835ee751369cc44e0
Matthew Dodds <mdodds92@hotmail.co.uk>
2021-03-20 21:51:38 +0000

    -Added functionality to CreateBlog. It now saves the blog to the DB but currently offers no user feedback of this fact. -Disabled the green selection as it broke my markdown editor

--- src/App.js
+++ src/App.js
@@ -11,51 +11,71 @@ import Chat from './chat/chat.js';
 import PageNotFound from './PageNotFound.js';
 import BlogHome from "./blog/BlogHome";
 import CreateBlog from "./blog/CreateBlog";
+import React from "react";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#0ed862",
+            contrastText: "#fff"
+        },
+        secondary: {
+            main: "#d8550e",
+            contrastText: "#fff"
+        },
+        error: {
+            main: "#f44336",
+            contrastText: "#fff"
+        }
+    },
+    spacing: v => `${v}em`
+});
 
 function App() {
 	return (
         <Router basename="/">
             <div className="App">
-                <Header img="" />
-
-                <Switch>
-                    <Route exact path="/">
-                        <Nav page="homepage" />
-                        <Homepage />
-                    </Route>
-                    <Route exact path="/blog">
-                        <Nav page="blog" />
-                        <BlogHome/>
-                    </Route>
-                    <Route exact path="/createblog">
-                        <Nav page="createblog" />
-                        <CreateBlog/>
-                    </Route>
-                    <Route exact path="/leaderboards">
-                        <Nav page="leaderboards" />
-                        <Leaderboard />
-                    </Route>
-                    <Route exact path="/rules">
-                        <Nav page="rules" />
-                        <Rules />
-                    </Route>
-                    <Route exact path="/chat">
-                        <Nav page="chat" />
-                        <Chat />
-                    </Route>
-                    <Route exact path="/map">
-                        <Nav page="map" />
-                        <Map />
-                    </Route>
-                    <Route exact path="/login">
-                        <Nav page="login" />
-                        {/* <Login/> */}
-                    </Route>
-                    <Route path="*">
-                        <PageNotFound/>
-                    </Route>
-                </Switch>
-
+                <MuiThemeProvider theme={theme}>
+                    <Header img="" />
+                    <Switch>
+                        <Route exact path="/">
+                            <Nav page="homepage" />
+                            <Homepage />
+                        </Route>
+                        <Route exact path="/blog">
+                            <Nav page="blog" />
+                            <BlogHome/>
+                        </Route>
+                        <Route exact path="/createblog">
+                            <Nav page="createblog" />
+                            <CreateBlog/>
+                        </Route>
+                        <Route exact path="/leaderboards">
+                            <Nav page="leaderboards" />
+                            <Leaderboard />
+                        </Route>
+                        <Route exact path="/rules">
+                            <Nav page="rules" />
+                            <Rules />
+                        </Route>
+                        <Route exact path="/chat">
+                            <Nav page="chat" />
+                            <Chat />
+                        </Route>
+                        <Route exact path="/map">
+                            <Nav page="map" />
+                            <Map />
+                        </Route>
+                        <Route exact path="/login">
+                            <Nav page="login" />
+                            {/* <Login/> */}
+                        </Route>
+                        <Route path="*">
+                            <PageNotFound/>
+                        </Route>
+                    </Switch>
+                </MuiThemeProvider>
                 <Footer />
             </div>
         </Router>
--- src/utils/Endpoints.js
+++ src/utils/Endpoints.js
@@ -1,8 +1,9 @@
 class Endpoints {
-   //static BASE = 'http://localhost:8081/';
-    static BASE = 'https://api.minecrossing.xyz/';
+    static BASE = 'http://localhost:8081/';
+   // static BASE = 'https://api.minecrossing.xyz/';
     static BLOG_POSTS = this.BASE + 'blogposts/';
     static BLOG_POSTS_PREVIEW = this.BLOG_POSTS + 'preview/';
+    static BLOG_POSTS_CREATE = this.BLOG_POSTS + 'create/';
 }
 
 export default Endpoints;
