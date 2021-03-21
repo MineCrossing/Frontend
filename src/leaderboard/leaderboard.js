581feb749a381e9575f71322fd499000954764e1
EthanCopeland <ethancopeland760@gmail.com>
2021-03-21 16:50:21 +0000

    we have leaderboard

--- src/leaderboard/leaderboard.js
+++ src/leaderboard/leaderboard.js
@@ -13,14 +13,29 @@ export default class Leaderboard extends React.Component {
 	}
 
 	render() {
-
 		let players = this.state.data;
+
+		let table = [];
+		for (const data of players) {
+			table.push(
+				<tr>
+					<td>{players.indexOf(data) + 1}</td>
+					<td>{data.name}</td>
+					<td>{data.level}</td>
+					<td>{data.kills}</td>
+					<td>{data.deaths}</td>
+					<td>{data.wins}</td>
+					<td>{data.losses}</td>
+					<td>{data.quests}</td>
+				</tr>
+			);
+		}
 
 		return (
 			<div id="homepage">
 				<div id="content" className="pure-u-3-5">
 
-					<h1 class="rules-header">Welcome to the Leaderboard!</h1>
+					<h1 className="rules-leaderboard">Welcome to the Leaderboard!</h1>
 
 					<table className="table" cellSpacing="0">
 						<thead>
@@ -36,6 +51,7 @@ export default class Leaderboard extends React.Component {
 							</tr>
 						</thead>
 						<tbody>
+							{table}
 						</tbody>
 					</table>
 					<br />
@@ -45,7 +61,7 @@ export default class Leaderboard extends React.Component {
 	}
 
 	componentDidMount() {
-		let url = "https://api.minecrossing.xyz/players";
+		let url = "http://localhost:8081/players";
 
 		fetch(url)
 			.then((response) => response.json())
