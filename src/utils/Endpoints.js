2b8660fecb3f2eaa54a44be72f67daafea5e796d
Matthew Dodds <mdodds92@hotmail.co.uk>
2021-03-21 22:12:19 +0000

    Switched endpoints to live

--- src/utils/Endpoints.js
+++ src/utils/Endpoints.js
@@ -1,6 +1,6 @@
 class Endpoints {
-    static BASE = 'http://localhost:8081/';
-   // static BASE = 'https://api.minecrossing.xyz/';
+    //static BASE = 'http://localhost:8081/';
+    static BASE = 'https://api.minecrossing.xyz/';
     static BLOG_POSTS = this.BASE + 'blogposts/';
     static BLOG_POSTS_PREVIEW = this.BLOG_POSTS + 'preview/';
     static BLOG_POSTS_PREVIEW_ALL = this.BLOG_POSTS + 'previewAll/';
