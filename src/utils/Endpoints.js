3f225a76e59421b31d4bb61e5c46f323decfb52b
Matthew Dodds <mdodds92@hotmail.co.uk>
2021-03-28 23:56:01 +0100

    magno cerebri

--- src/utils/Endpoints.js
+++ src/utils/Endpoints.js
@@ -1,6 +1,6 @@
 class Endpoints {
-    static BASE = 'http://localhost:8081/';
-    //static BASE = 'https://api.minecrossing.xyz/';
+    //static BASE = 'http://localhost:8081/';
+    static BASE = 'https://api.minecrossing.xyz/';
 
     static BLOG_POSTS = this.BASE + 'blogposts/';
     static BLOG_POSTS_PREVIEW = this.BLOG_POSTS + 'preview/';
